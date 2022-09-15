e97a02d80714b929e04e0267a7bbc7a081c55469
DESKTOP-2AG9GJT\Alejandro Cruz <alejandroxdlx@gmail.com>
2022-09-15 10:11:09 +0100

    Este es nuestro segundo commit

--- src/main/java/com/mycompany/kata1/Kata1.java
+++ src/main/java/com/mycompany/kata1/Kata1.java
@@ -1,10 +1,16 @@
 package com.mycompany.kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
+    
     public static void main(String[] args) {
-        Person persona = new Person("Zacarías", new Date(102,10,25));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2002, 8, 14);
+        Person persona = new Person("Zacarías", date);
         System.out.println(persona.getName() + " tiene " + persona.getAge() + " años.");
     }
 }
